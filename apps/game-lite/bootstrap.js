3f05902d3bface0bc0391057da0fe2ab74a8085b
FIREGRAM <olayemiqudus21@gmail.com>
2026-08-07 10:04:34 +0100

    Wire navigation pose into field HUD

--- apps/game-lite/bootstrap.js
+++ apps/game-lite/bootstrap.js
@@ -1,7 +1,9 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.js';
 import { installLegacyBuildingWallUpgrade } from './buildingWalls.js';
+import { installNavigationBridge } from './navigation-bridge.js';
 import { installWallSurfaceSwap } from './wallSurfaceSwap.js';
 
+installNavigationBridge(THREE);
 installLegacyBuildingWallUpgrade(THREE);
 installWallSurfaceSwap(THREE);
 await import('./game.js');
