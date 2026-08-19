ed025ed1cdf77242a58119ccc21e1a21cd0f741c
FIREGRAM <olayemiqudus21@gmail.com>
2026-08-19 12:58:47 +0100

    fix Quality+Vercel: prettier + static vercel.json (index.js)

--- apps/game-lite/core/index.js
+++ apps/game-lite/core/index.js
@@ -1,4 +1,4 @@
-export { eventBus } from './EventBus.js';
-export { Events } from './Events.js';
-export { gameState } from './GameState.js';
-export { game, Game } from './Game.js';
+export { eventBus } from "./EventBus.js";
+export { Events } from "./Events.js";
+export { gameState } from "./GameState.js";
+export { game, Game } from "./Game.js";
