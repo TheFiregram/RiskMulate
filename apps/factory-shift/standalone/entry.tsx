82ba03c11ea9107c8f05ff8ffaa0af884e4a1c23
FIREGRAM <olayemiqudus21@gmail.com>
2026-08-27 23:04:40 +0100

    Wire mission-console upgrades into standalone release

--- apps/factory-shift/standalone/entry.tsx
+++ apps/factory-shift/standalone/entry.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
-import FactoryExperience from "../app/factory-experience";
+import ExperienceUpgrades from "../app/experience-upgrades";
 import "../app/globals.css";
+import "../app/apex-shell.css";
 
 const mount = document.getElementById("riskmulate-root");
 
 if (!mount) throw new Error("RiskMulate mount point is missing.");
 
-createRoot(mount).render(<FactoryExperience />);
+createRoot(mount).render(<ExperienceUpgrades />);
